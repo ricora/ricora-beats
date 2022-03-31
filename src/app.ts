207ac552203c1cdbc5bec33abd064c0dbc93374a
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-03-31 19:54:51 +0900

    🎉 npm init

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import * as Phaser from "phaser"
+
+class MyScene extends Phaser.Scene {
+    constructor() {
+        super("myscene")
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        this.add.text(640, 360, "Hello World", { fontFamily: "arial", fontSize: "60px" }).setOrigin(0.5)
+    }
+}
+
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    parent: "game-app",
+    scene: MyScene
+}
+
+new Phaser.Game(config)
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+    entry: {
+        bundle: "./src/app.ts"
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist")
+    },
+    module: {
+        rules: [{
+            test: /\.ts$/,
+            loader: "ts-loader"
+        }]
+    }
+}
