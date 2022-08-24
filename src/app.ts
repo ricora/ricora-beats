1b3205af42a4795797eec554d954fbd38e992aa9
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-08-24 22:15:19 +0900

    ✨ Canvasのサイズを可変に

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,17 @@
 import * as Phaser from "phaser"
 import { Scenes } from './scene'
 
-class MyScene extends Phaser.Scene {
-    constructor() {
-        super("myscene")
-    }
 
-    preload() {
 
-    }
-
-    create() {
-        this.add.text(640, 360, "Hello World", { fontFamily: "arial", fontSize: "60px" }).setOrigin(0.5)
-    }
-}
 
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: "game-app",
+        width: 1280,
+        height: 720
+    },
     parent: "game-app",
     scene: Scenes
 }
