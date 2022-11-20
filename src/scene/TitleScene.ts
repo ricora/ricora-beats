a08f018510cc9eb2c3d9d4a77f428262d0c95a03
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-11-21 01:41:12 +0900

    ✨ 仮のタイトルシーンを実装

--- src/scene/TitleScene.ts
+++ src/scene/TitleScene.ts
@@ -2,6 +2,10 @@ import { DebugGUI } from "../class/DebugGUI"
 
 export class TitleScene extends Phaser.Scene {
     private debugGUI: DebugGUI
+
+    private startText: Phaser.GameObjects.Text
+
+    private backgroundCamera: Phaser.Cameras.Scene2D.Camera
     constructor() {
         super("title")
     }
@@ -16,14 +20,48 @@ export class TitleScene extends Phaser.Scene {
     create() {
         const { width, height } = this.game.canvas
 
-        this.add.text(0, 0, "title scene")
+        this.backgroundCamera = this.cameras.add(0, 0, 1280, 720)
+        this.backgroundCamera.setScroll(1280, 720)
+        this.cameras.add(0, 0, 1280, 720, true)
+        this.add
+            .shader("background", width / 2 + 1280, height / 2 + 720, 1280, 720)
+            .setDepth(-5)
 
-        const zone = this.add.zone(width / 2, height / 2, width, height)
-        zone.setInteractive({
-            useHandCursor: true,
+        // @ts-expect-error
+        this.plugins.get("rexKawaseBlurPipeline").add(this.backgroundCamera, {
+            blur: 8,
+            quality: 8,
         })
-        zone.on("pointerdown", () => {
-            //this.scene.start("select")
-        })
+
+        this.startText = this.add
+            .text(640, 520, "touch to start", {
+                fontFamily: "Bungee",
+                fontSize: "80px",
+                color: "#fafafa",
+                align: "center",
+            })
+            .setOrigin(0.5)
+            .setDepth(10)
+            .setAlpha(1)
+            .setScale(0.5)
+            .setInteractive({
+                useHandCursor: true,
+            })
+            .on("pointerdown", () => {
+                this.cameras.main.fadeOut(800)
+            })
+        this.cameras.main.once(
+            Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE,
+            () => {
+                this.scene.start("select")
+            }
+        )
+        this.cameras.main.fadeIn(500)
+    }
+
+    update(time: number, dt: number) {
+        this.startText.setAlpha(
+            0.5 + 0.5 * 0.5 * (0.25 * Math.sin((time * 2 * Math.PI) / 1000) + 1)
+        )
     }
 }
