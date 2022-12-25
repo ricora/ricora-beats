0d3b796cb805bb00c246af2cf048dc3941a989db
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2022-12-25 16:53:45 +0900

    ⚙ 環境変数にランキングサーバーのURL`SERVER_URL`を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
-const path = require("path");
+const path = require("path")
+const webpack = require("webpack")
 module.exports = {
     entry: {
-        bundle: "./src/app.ts"
+        bundle: "./src/app.ts",
     },
     output: {
         path: path.join(__dirname, "dist"),
-        filename: "[name].js"
+        filename: "[name].js",
     },
     resolve: {
-        extensions: [".ts", ".js"]
+        extensions: [".ts", ".js"],
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env.SERVER_URL": JSON.stringify(process.env.SERVER_URL),
+        }),
+    ],
     devServer: {
         static: {
-            directory: path.join(__dirname, "dist")
-        }
+            directory: path.join(__dirname, "dist"),
+        },
     },
     module: {
         rules: [{
             test: /\.ts$/,
-            loader: "ts-loader"
-        }]
-    }
+            loader: "ts-loader",
+        }, ],
+    },
 }
