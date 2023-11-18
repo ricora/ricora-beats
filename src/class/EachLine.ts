6fe33d0897bc6da976664324ef084044077dccae
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2023-11-18 20:45:15 +0900

    同時押しガイドを実装

---
+++ src/class/EachLine.ts
@@ -0,0 +1,9 @@
+export class EachLine {
+  public beat: number
+  public rectangle: Phaser.GameObjects.Rectangle
+
+  constructor(beat: number, rectangle: Phaser.GameObjects.Rectangle) {
+    this.beat = beat
+    this.rectangle = rectangle
+  }
+}
